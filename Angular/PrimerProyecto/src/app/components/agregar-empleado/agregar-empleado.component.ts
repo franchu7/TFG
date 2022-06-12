ccebc81e56be1dd35e7f9fd0f270e3f2ac2c35c7
franchu7 <73340817+franchu7@users.noreply.github.com>
2022-06-12 18:04:37 +0200

    Primera parte, primer proyecto con Angular.

---
+++ Angular/PrimerProyecto/src/app/components/agregar-empleado/agregar-empleado.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-empleado',
+  templateUrl: './agregar-empleado.component.html',
+  styleUrls: ['./agregar-empleado.component.css']
+})
+export class AgregarEmpleadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  enviarDatos(): any {
+    console.log("Me presionaste");
+  }
+
+}
