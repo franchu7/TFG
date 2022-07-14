351e9835db262d73b49ab17955a53a158ca92b3e
franchu7 <73340817+franchu7@users.noreply.github.com>
2022-07-15 01:26:23 +0200

    Interfaz de Login

---
+++ Angular/ClassroomApp/src/app/modules/auth/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginData } from 'src/app/core/models/auth-data';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  public loginForm: FormGroup;
+  public formSubmitted: boolean;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
+        ]
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(20),
+        ]]
+    });
+    this.formSubmitted = false;
+  }
+
+  ngOnInit(): void {
+  }
+
+  public submitForm(): void {
+    this.formSubmitted = true;
+    if(this.loginForm.invalid) {
+      console.log('ERROR EN EL FORMULARIO');
+      return;
+    }
+    const data: LoginData = this.loginForm.value;
+    console.log(data);
+  }
+
+
+}
