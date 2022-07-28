b2577759a1a84a5f1e3f11642e2a72b3e13d0bbc
franchu7 <73340817+franchu7@users.noreply.github.com>
2022-07-29 01:39:34 +0200

    Update classroom.sql

--- Base de datos/classroom.sql
+++ Base de datos/classroom.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-07-2022 a las 18:12:04
+-- Tiempo de generación: 29-07-2022 a las 01:38:28
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -52,11 +52,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `name`, `surname1`, `surname2`, `dni`, `gender`, `street`, `streetNum`, `floor`, `zipCode`, `location`, `province`, `phoneNum`, `role`, `avatar`) VALUES
-(1, 'admin@gmail.com', '$2y$10$.UtmhPn94fgYwV4NF80e4.LS9O003cuO7A/qXt5c0EPcNI1VQ5My2', 'Francisco', 'Calles', 'Esteban', '09050894A', 'hombre', 'Av. Juan de Austria', '12', '6ºD', '28805', 'Alcalá de Henares', 'Madrid', '686597257', 'admin', 'assets/img/background.jpg'),
+(1, 'admin@gmail.com', '$2y$10$70oDndcCYBhBLUQIFxibGuqyNBlQ4is3wju10XD1Kvz0cr6PcTjAW', 'Francisco', 'Calles', 'Esteban', '09050894A', 'hombre', 'Av. Juan de Austria', '12', '6ºD', '28805', 'Alcalá de Henares', 'Madrid', '686597257', 'admin', 'assets/img/background.jpg'),
 (2, 'user1@gmail.com', '$2y$10$ptzsHPwcAX/A/fO6ve6p8.xGQ7tHB.ZnFHm0QMGe5.R5.zyn2w5My', 'Pepito', 'Pérez', 'Gutíerrez', '35354543B', 'hombre', 'C. CalleA', '1', '', '28805', 'Alcalá de Henares', 'Madrid', '665463435', 'student', 'assets/img/user-avatar.png'),
 (3, 'user2@gmail.com', '$2y$10$ER/vkPRccR7s8N3h6hvD5.TcMf0LZoQ7LMNL9E0lg5yawfYPSc2xy', 'Marta', 'García', 'López', '35354543C', 'mujer', 'C. CalleA', '2', '', '28805', 'Alcalá de Henares', 'Madrid', '600934435', 'student', 'assets/img/user-avatar.png'),
-(4, 'user3@gmail.com', '$2y$10$XpXhRuZb3mJl.IZ1moHFDejhulWmYIuybTnmNPZQp0OIYRyZvRhCe', 'Adrian', 'Calles', 'Esteban', '00945373D', 'hombre', 'C. CalleA', '3', '', '28805', 'Alcalá de Henares', 'Madrid', '666342981', 'student', 'assets/img/user-avatar.png'),
-(5, 'p.solis@gmail.com', '$2y$10$nh3Dzx6zyhO3UKdQ5iEWaO6HkUqFpPdNUdqQjROIlEmQJvhsAUwXe', 'Paula', 'Solis', '', '04346214G', 'mujer', 'Vía Complutense', '30', '4ºB', '28805', 'Alcalá de Henares', 'Madrid', '659264901', 'student', 'assets/img/user-avatar.png');
+(6, 'm.tejada@gmail.com', '$2y$10$NwG/NBBaQozsAp/oETYRreomqVL66FvCXJ2NULSmUqJ.5HtaWooHq', 'Marcos', 'Tejada', 'Sanz', '34535465A', 'hombre', 'C. Hortaleza', '45', '', '29844', 'Móstoles', 'Madrid', '624574825', 'student', 'assets/img/user-avatar.png'),
+(7, 'patry.r@gmail.com', '$2y$10$LR/PkCAlm46R4ZnWYg4MMeFVyWezDpnzs.Q8uDDjC.xXlVCgq0eUG', 'Patricia', 'Rodríguez', 'Jiménez', '05958545G', 'otro', 'Vía Calle', '568', '', '42434', 'Torrejón de Ardoz', 'Madrid', '635456456', 'student', 'assets/img/user-avatar.png');
 
 --
 -- Índices para tablas volcadas
@@ -77,7 +77,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
